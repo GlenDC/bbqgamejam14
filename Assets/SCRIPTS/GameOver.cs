43a43a650adf1b13c7b444e80226c65c7c41e194
glendc <decauwsemaecker.glen@gmail.com>
2014-08-31 08:43:13 +0000

    player flow complete

--- Assets/SCRIPTS/GameOver.cs
+++ Assets/SCRIPTS/GameOver.cs
@@ -4,19 +4,42 @@ using System.Collections;
 
 public class GameOver : MonoBehaviour {
 	private Text gameOverTitle;
+	private Text gameOverInfo;
 	private int winnerID;
+
+	private float currentTime;
 
 	// Use this for initialization
 	void Awake () {
 		gameOverTitle = GameObject.Find("GameOverTitle").GetComponent<Text>();
-		//TEMP/////////////////////////
-		winnerID = 1;
-		gameOverTitle.text = "PLAYER " + "X" + " WINS";
+		gameOverInfo = GameObject.Find("GameOverInfo").GetComponent<Text>();
+
+		currentTime = 0.0f;
+
+		gameOverInfo.text = "";
 	}
 
 	public void SetWinner(int id)
 	{
 		winnerID = id;
 		gameOverTitle.text = "PLAYER " + winnerID + " WINS";
+	}
+
+	void Update()
+	{
+		if(currentTime < 3.0f)
+		{
+			currentTime += Time.deltaTime;
+			int countDown = 3 - (int)currentTime;
+			gameOverInfo.text = "Press the START button in <" + countDown + "> to meat again!";
+		}
+		else
+		{
+			gameOverInfo.text = "Press the START button to meat again!";
+			if(Input.anyKey)
+			{
+				Application.LoadLevel("menu");
+			}
+		}
 	}
 }
