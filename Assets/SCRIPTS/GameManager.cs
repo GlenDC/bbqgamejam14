ed6ad5cbf6c8413ee87d57412c03079373bbc200
glendc <decauwsemaecker.glen@gmail.com>
2014-08-31 02:21:02 +0000

    xtra merge

--- Assets/SCRIPTS/GameManager.cs
+++ Assets/SCRIPTS/GameManager.cs
@@ -5,23 +5,17 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
 
 	private GameObject pauseMenu;
-<<<<<<< HEAD
 	public static bool IsPaused = false;
-=======
 	private GameObject gameOverMenu;
->>>>>>> 03cccdc6dabc0d0e89a126aca1c6fd851588f7c9
 
 	// Use this for initialization
 	void Start () {
 		pauseMenu = GameObject.Find("PauseMenu");
-<<<<<<< HEAD
 		pauseMenu.SetActive (false);
 		IsPaused = false;
-=======
 		gameOverMenu = GameObject.Find("GameOverMenu");
 		pauseMenu.SetActive(false);
 		gameOverMenu.SetActive(false);
->>>>>>> 03cccdc6dabc0d0e89a126aca1c6fd851588f7c9
 	}
 	
 	// Update is called once per frame
